7511cbc085fa8afe78703dd5af4aabe58e2e0c6b
Manoj <manojshivaprakash10@gmail.com>
2024-07-04 11:46:30 +0530

    Get Nth node from the end of the LL

--- test.java
+++ test.java
@@ -1,6 +1,38 @@
 class test{
 
+    private ListNode head;
+    private static class ListNode{
+        private int data;
+        private ListNode next;
+
+        public ListNode(int data){
+            this.data = data;
+            this.next = null;
+        }
+
+    }
+
+    public void displayNode(){
+        ListNode current = head;
+        while(current != head){
+            System.out.println(current.data + " --> ");
+            current = current.next;
+        }
+        System.out.println("null");
+    }
+
     public static void main(String[] args) {
-        System.out.println("This is a test code");
+        test tst = new test();
+        tst.head = new ListNode(10);
+        ListNode second = new ListNode(10);
+        ListNode third = new ListNode(10);
+        ListNode fourth = new ListNode(10);
+        
+        tst.head.next = second;
+        second.next = third;
+        third.next = fourth;
+
+        
+        tst.displayNode();
     }
 }
